ba65f78d8e55d03cefba9688743465881544e874
TJR181 <1552019573@qq.com>
2025-07-08 15:24:09 +0800

    learn solidity reverts

--- learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
+++ learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
@@ -6,8 +6,15 @@
 pragma solidity ^0.8.19;
 
 contract FundMe{
+
+    uint256 public myValue = 1;
+
     function fund() public payable{
+        myValue = myValue +2;
         require(msg.value > 1e18 , "didn't send enough ETH");
+
+        // What is revert?
+        // revert is undo any actions that have been done , and send the remaining gas back
     }
 
     function withdraw() public{
