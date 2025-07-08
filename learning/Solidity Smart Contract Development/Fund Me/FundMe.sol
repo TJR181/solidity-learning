32cb375adbfb9b5c7c6a56e56cc8188ca9abed85
TJR181 <1552019573@qq.com>
2025-07-08 17:39:00 +0800

    solidity interfaces

--- learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
+++ learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
@@ -5,6 +5,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe{
 
     uint256 minimumUSD = 5;
@@ -16,6 +33,16 @@ contract FundMe{
 
         // What is revert?
         // revert is undo any actions that have been done , and send the remaining gas back
+    }
+
+    function getPrice() public{
+        // Address = 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        
+    }
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
     function withdraw() public{
