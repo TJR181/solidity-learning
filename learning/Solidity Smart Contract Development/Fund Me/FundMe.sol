426de9e2dca3a8748a8b4fdecfe0b402adfbae1a
TJR181 <1552019573@qq.com>
2025-07-08 13:46:27 +0800

    fund me project setup

---
+++ learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
@@ -0,0 +1,16 @@
+// Get funds from users
+// Withdraw Funds 
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract FundMe{
+    function fund() public{
+
+    }
+
+    function withdraw() public{
+        
+    }
+}
