2612df936e808c72f817f15288d9c3277907d97f
TJR181 <1552019573@qq.com>
2025-07-13 15:40:11 +0800

    creating my own libraries

--- learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
+++ learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
@@ -5,9 +5,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
  
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+//import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
@@ -16,31 +18,14 @@ contract FundMe{
 
     function fund() public payable{
         //myValue = myValue +2;
-        require(getConversionRate(msg.value) >= minimumUSD , "didn't send enough ETH");
-
+        //require(getConversionRate(msg.value) >= minimumUSD , "didn't send enough ETH");
+        require(msg.value.getConversionRate() >= minimumUSD , "didn't send enough ETH");
         // What is revert?
         // revert is undo any actions that have been done , and send the remaining gas back↑
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function getPrice() public view returns (uint256){
-        // Address = 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // Price in ETH in terms USD
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmouht) public view returns (uint256){
-        uint256 usdAmount = ethAmouht * getPrice() / 1e18;
-        return usdAmount;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
 
     function withdraw() public{
 
