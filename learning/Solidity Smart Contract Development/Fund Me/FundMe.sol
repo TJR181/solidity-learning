290f1ef9f66fdd361068e6832f0be987c6da82ca
TJR181 <1552019573@qq.com>
2025-07-14 14:15:00 +0800

    Sending ETH from a contract

--- learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
+++ learning/Solidity Smart Contract Development/Fund Me/FundMe.sol
@@ -34,5 +34,23 @@ contract FundMe{
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
+
+        // Three different ways to send native cryptocurrency
+        // Transfer
+        // Send
+        // Call
+
+        // 1.Transfer
+        // msg.sender is an address type
+        // payable(msg.sender) is a payable address type
+        payable(msg.sender).transfer(address(this).balance);
+
+        // 2.Send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess , "send failed!");
+
+        // 3.Call
+        (bool callSuccess , ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess , "call failed!");
     }
 }
