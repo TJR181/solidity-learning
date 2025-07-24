731271ae5be98a77da44fa9779f5e102f66c1eee
TJR181 <1552019573@qq.com>
2025-07-24 19:24:00 +0800

    format code by using : forge fmt

--- learning/Foundry_Fundamentals/foundry-simple-storage-f23/script/DeploySimpleStorage.s.sol
+++ learning/Foundry_Fundamentals/foundry-simple-storage-f23/script/DeploySimpleStorage.s.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity^0.8.19;
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage is Script{
-    function run() external returns(SimpleStorage){
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
         vm.startBroadcast();
         SimpleStorage simplestorage = new SimpleStorage();
         vm.stopBroadcast();
